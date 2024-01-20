940801f99bad3cc80d360636ca7a40eb12b2c514
Somnath <107599122+MrSomnathRoy@users.noreply.github.com>
2024-01-20 11:50:04 +0530

    Update Home2.jsx

--- src/components/Home/Home2.jsx
+++ src/components/Home/Home2.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-// import myImg from "../../Assets/avatar.svg";
 import myImg from "../../Assets/My Photo.jpg";
 
 import Tilt from "react-parallax-tilt";
