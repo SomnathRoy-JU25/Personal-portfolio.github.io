6ff3c7219b6597e74e0969d019f42012be2de2e9
Somnath Roy <somnathroy0340@gmail.com>
2024-03-29 13:46:18 +0530

    Update

--- src/components/ContactUs/ContactForm.jsx
+++ src/components/ContactUs/ContactForm.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import { useRef } from "react";
-import emailjs from "ailjs/browser@em";
+import emailjs from "@emailjs/browser";
 import {toast} from "react-hot-toast";
 
 import CountryCode from "../../data/countrycode.json";
